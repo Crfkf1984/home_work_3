745d9fa119741a2ce7a84ae441a0900a1957b78a
Sergey Kotovich <seirosso1@gmail.com>
2024-08-03 22:36:38 +0300

    Two test

--- src/test/java/MouseHoverMove.java
+++ src/test/java/MouseHoverMove.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -24,5 +25,14 @@ public class MouseHoverMove {
         $$(".HeaderMenu-item").get(1).shouldHave(Condition.text("Solutions")).hover();
         $$(".border-bottom ul li").get(0).$("a").click();
         $("[data-testid='Grid-:Ra9b:']").shouldHave(text("The AI-powered developer platform."));
+    }
+
+    @Test
+    public void dragAndDropTest() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        open("/drag_and_drop");
+        $("#column-b").shouldHave(text("B"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-b").shouldHave(text("A"));
     }
 }
