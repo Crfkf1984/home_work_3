31dbc6383b04837ff7b2d688d97b1dedb3c1a4ea
Sergey Kotovich <seirosso1@gmail.com>
2024-08-03 22:38:07 +0300

    chance before all

--- src/test/java/MouseHoverMove.java
+++ src/test/java/MouseHoverMove.java
@@ -12,11 +12,9 @@ public class MouseHoverMove {
 
     @BeforeAll
     public static void before() {
-        System.setProperty("webdriver.chrome.driver", "C:\\.cache\\selenium\\chromedriver\\win64\\126.0.6478.126\\chromedriver.exe");
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
     }
 
     @Test
